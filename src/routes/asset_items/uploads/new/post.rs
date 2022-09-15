175cee4fbe284eabfd0a5ed256c2a7887481d705
Reed Elam <relam@SBO-ITLAP-04.rcschools.us>
2022-09-15 15:36:07 -0400

    add multipart csv upload

--- src/routes/asset_items/uploads/new/post.rs
+++ src/routes/asset_items/uploads/new/post.rs
@@ -1,16 +1,16 @@
 use actix_web::http::StatusCode;
 use actix_web::http::header::ContentType;
 use actix_web::{web, HttpResponse, ResponseError};
-use actix_web_lab::__reexports::futures_util::TryStreamExt;
-use anyhow::Context;
 use sqlx::{PgPool, Postgres, Transaction};
 use validator::Validate;
 use chrono::Utc;
 use actix_web_flash_messages::FlashMessage;
 use actix_multipart::{Multipart, Field};
+use futures::{StreamExt, TryStreamExt};
+use std::io::Write;
 
 use crate::telemetry::spawn_blocking_with_tracing;
-use crate::utils::{error_chain_fmt, see_other};
+use crate::utils::{error_chain_fmt, see_other, e500};
 use crate::domain::Asset;
 
 
@@ -27,11 +27,23 @@ pub async fn upload_assets(
         let content_type = field.content_disposition();
 
         //let filename = content_type.get_filename().unwrap();
-        let filepath = format!("/temp/{}.csv", uuid::Uuid::new_v4());
+        let filepath = format!("./temp_files/{}.csv", uuid::Uuid::new_v4());
         //FlashMessage::success(filepath).send();
 
         // File::create is blocking operation
-        
+        let mut f = spawn_blocking_with_tracing(move || std::fs::File::create(filepath))
+            .await
+            .map_err(e500)??;
+
+        // Field in turn is stream of *Bytes* object
+        while let Some(chunk) = field.next().await {
+            let data = chunk?;
+
+            // filesystem operations are blocking
+            f = spawn_blocking_with_tracing(move|| f.write_all(&data).map(|_| f))
+                .await
+                .map_err(e500)??;
+        }
     }
 
     Ok(see_other("/asset_items/uploads/new"))
